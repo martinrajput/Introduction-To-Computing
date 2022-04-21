db780a6c999e36331080ac90fbd1c972fa9f104f
martinrajput <104158726+martinrajput@users.noreply.github.com>
2022-04-21 22:02:18 +0530

    Add files via upload

---
+++ Assignment1solution.py
@@ -0,0 +1,30 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================================================= RESTART: C:/Users/DEEPESH KUMAR/Desktop/Assignment1.py ================================================
+QUESTION 1 :
+ENTER A NUMBER : 34
+ENTER ANOTHER NUMBER : 23
+ENTER ANOTHER NUMBER : 17
+24.666666666666668
+QUESTION 2 :
+ENTER GROSS INCOME : 300000
+ENTER NUMBER OF DEPENDENTS : 4
+55600.0
+QUESTION 3 :
+ENTER YOUR SID : 21102119
+ENTER YOUR NAME : martin
+ENTER YOUR GENDER : male
+ENTER YOUR COURSE NAME : ES1101
+ENTER YOUR CGPA9.7
+[21102119, 'martin', 'male', 'ES1101', 9.7]
+QUESTION 4 :
+ENTER MARKS OF FIRST STUDENT 32
+ENTER MARKS OF SECOND STUDENT 29
+ENTER MARKS OF THIRD STUDENT90
+ENTER MARKS OF FOURTH STUDENT 54
+ENTER MARKS OF FIFTH STUDENT18
+[18, 29, 32, 54, 90]
+QUESTION 5 :
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
