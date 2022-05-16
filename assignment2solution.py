7d37f9bc80513dbbd5e9adec15bb690f1e8e0e98
martinrajput <104158726+martinrajput@users.noreply.github.com>
2022-05-16 11:36:04 -0700

    Add files via upload

---
+++ assignment2solution.py
@@ -0,0 +1,39 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: C:/Users/hp/Desktop/assignment2.py =================
+QUESTION 1.)
+PART A
+35
+PART B
+egaugnal evitisnes esac a si nohtyP
+PART C
+SLICED STRING IS:  a case sensitive
+PART D
+Python is object oriented language
+PART E
+10
+PART F
+Pythonisacasesensitivelanguage
+QUESTION 2.)
+Hey,Martin Here!
+My SID is 21102119
+I am from CIVIL department and my CGPA is 9.9
+QUESTION 3.)
+a&b is:  8
+a|b is:  58
+a^b is:  50
+a<<2: 224 	b<<2:  40
+a>>2: 14 	b>>4:  0
+QUESTION 4.)
+ENTER ANY STRING : myname is martin
+YES
+QUESTION 5.)
+ENTER LENGTH OF FIRST SIDE OF TRAINGLE: 34
+ENTER LENGTH OF SECOND SIDE OF TRAINGLE: 56
+ENTER LENGTH OF THIRD SIDE OF TRAINGLE: 44
+YES
+QUESTION 6.)
+enter a number : 23
+enter another number : 34
+4
