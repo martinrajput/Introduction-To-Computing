888df28b779be38e777b16c0d370f53d7e7acaef
martinrajput <104158726+martinrajput@users.noreply.github.com>
2022-06-11 06:13:56 -0700

    Add files via upload

---
+++ assgn 3 output.py
@@ -0,0 +1,40 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======== RESTART: C:\Users\laptop\Desktop\Assignment 3\Assign3 input.py ========
+Enter a number to convert it into its binary equivalent:34
+0100010
+Select an operation to perform : 
+1. ADD
+2. SUBTRACT
+3. MULTIPLY
+4. DIVIDE
+3
+Enter first number: 23
+Enter second number: 67
+The product is :1541
+Enter the value of a :23
+Enter the value of b :34
+Enter the value of c :12
+Ans = 684
+Enter the value of n :34
+Ans = 561.0
+radius_of_sphere19
+Ans = 4536.459791783661
+radius20
+Ans = 4.47213595499958
+variable 112
+variable 223
+variable 324
+variable 412
+Ans = -0.9166666666666666
+0 1 2 3 4 
+3 4 5 6 7 8 9 
+4 7 10 
+15 13 11 9 7 
+7
+This program finds the total molecular weight of a combination of hydrogen, carbon and oxygen atoms :
+How many hydrogen atoms? 12
+Carbon? 2
+Oxygen? 3
+The total molecular weight is 84.11488
